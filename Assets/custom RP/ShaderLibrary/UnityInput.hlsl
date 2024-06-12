fea6644d0f18a4312153e874e3270425d1465e30
toastoffee <931391442@qq.com>
2024-06-12 13:35:50 +0800

    complete unlit shader

--- Assets/custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/custom RP/ShaderLibrary/UnityInput.hlsl
@@ -1,8 +1,17 @@
 #ifndef CUSTOM_UNITY_INPUT_INCLUDED
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+
 float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+real4 unity_WorldTransformParams;
 
 float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 unity_MatrixInvV;
+float4x4 unity_prev_MatrixM;
+float4x4 unity_prev_MatrixIM;
+float4x4 glstate_matrix_projection;
 
 #endif
